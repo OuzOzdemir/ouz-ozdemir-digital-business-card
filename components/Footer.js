b2908ad98025eb07b5ee005fdaf89112ab083a37
Oguzhan Ozdemir <ouzozdev@gmail.com>
2023-04-01 14:54:50 +0300

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,12 @@ import React from "react"
 export default function Main() {
     return (
         <footer className="footer-container">
-        <img className="footer-icon"  src="https://imgur.com/otq3xbt.png"/> 
-        <img className="footer-icon"  src="https://imgur.com/l89H2Yl.png" />
-        <img className="footer-icon"  src="https://imgur.com/PcrLUeG.png" />
-        <img className="footer-icon"  src="https://imgur.com/Ga2CKty.png" />
+            <div className="socials-container">
+                <a href="https://twitter.com/home"><img className="footer-icon"  src="https://imgur.com/otq3xbt.png"/> </a>
+                <a href="https://www.facebook.com/owlthegentleman/"><img className="footer-icon"  src="https://imgur.com/l89H2Yl.png" /></a>
+                <a href="https://www.instagram.com/owlthetyto/"><img className="footer-icon"  src="https://imgur.com/PcrLUeG.png" /></a>
+                <a href="https://github.com/OuzOzdemir"><img className="footer-icon"  src="https://imgur.com/Ga2CKty.png" /></a>
+            </div>
         </footer>
     )
 }
--- components/Main.js
+++ components/Main.js
@@ -3,22 +3,22 @@ import React from "react"
 export default function main() {
     return (
         <main className="main-container">
-        <img className="card-img" src="https://imgur.com/o6ww4TR.png" alt="card-img"></img>
-        <div className="name">Oguzhan Ozdemir</div>
-        <div className="title">Frontend Developer</div>
-        <div className="website">github.com/OuzOzdemir</div>
-        <div className="contact">
-        <div className="button1"><img className= "icon" src="https://imgur.com/9fPNmf5.png"/> Email</div>
-        <div className="button2"><img className= "icon" src="https://imgur.com/XfNd6Jp.png"/>Linkedin</div>
-        </div>
-        
-        <div className="about-container">
-        <div className="head-about">About</div>
-        <div className="text">I’ve graduated with a Marine Engineering Bachelor of Science Degree from Istanbul Technical University. I have been working on commercial oil/chemical tanker vessels since 2016 overseas. I decided to make a career change; therefore, I stopped my career at sea to become frontend developer in December 2022.
-        </div>
-        <div className="head-about">Interests</div>
-        <div className="text">Body building. Basketball. Coffee  Movies. Manga. Anime Series. Hiking. Cooking. Music.</div>
-        </div>
+            <img className="card-img" src="https://imgur.com/o6ww4TR.png" alt="card-img"></img>
+            <div className="name">Oguzhan Ozdemir</div>
+            <div className="title">Frontend Developer</div>
+            <div className="website">github.com/OuzOzdemir</div>
+            <div className="contact">
+                <div className="button1"><i className="fa-solid fa-envelope icon"></i><a href = "mailto:ouzozdev@gmail.com">Email</a></div>
+                <div className="button2"><i className="fa-brands fa-linkedin icon"></i><a href="https://github.com/OuzOzdemir">LinkedIn</a></div>
+            </div>
+
+            <div className="about-container">
+                <div className="head-about">About</div>
+                <div className="text">I’ve graduated with a Marine Engineering Bachelor of Science Degree from Istanbul Technical University. I have been working on commercial oil/chemical tanker vessels since 2016 overseas. I decided to make a career change; therefore, I stopped my career at sea to become frontend developer in December 2022.
+                </div>
+                <div className="head-about">Interests</div>
+                <div className="text">Body building. Basketball. Coffee. Movies. Manga. Anime Series. Hiking. Cooking. Music.</div>
+            </div>
         </main>
     )
 }
